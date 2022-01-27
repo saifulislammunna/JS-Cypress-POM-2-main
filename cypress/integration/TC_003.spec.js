c373762443317d5590d3d7a8a140e82fe0e7acd0
saifulislammunna <saifulmunna28@gmail.com>
2022-01-27 12:23:49 +0600

    sign-up-added

--- cypress/integration/TC_003.spec.js
+++ cypress/integration/TC_003.spec.js
@@ -4,4 +4,38 @@
 })
 
 
- 
+  it('SignUp test', function(){
+    const typedFirstName = 'Saiful Islam'
+    const typedLastName = 'Munna'
+    const typedAge = 23
+    const typedEmail = 'saifulmunna28@gmail.com'
+    const typedPassword = '+P5*M,@cjPRz$Bm'  
+
+    cy.get('.gn-signup > .gn-title').click()
+      
+    cy.get('.input-container > [data-testid="identity-first-name-input"]')
+    .type(typedFirstName)
+    .should('have.value', typedFirstName)
+
+    cy.get('.input-container > [data-testid="identity-last-name-input"]')
+    .type(typedLastName)
+    .should('have.value', typedLastName)
+
+    cy.get('[data-testid="no-test-id-given"]')
+    .type(typedAge)
+    .should('have.value', typedAge)
+
+     cy.get('[data-testid="identity-email-input"]')
+     .type(typedEmail)
+     .should('have.value', typedEmail)
+       
+     cy.get('[data-testid="identity-password-input"]') 
+     .type(typedPassword)
+     .should('have.value', typedPassword)
+
+    cy.get('[style="width: 304px; height: 78px;"] > div > iframe').click()   
+      /* .recaptcha-checkbox-border */  
+     cy.get('[data-testid="identity-form-submit-button"]').click()    
+      
+     
+})   
