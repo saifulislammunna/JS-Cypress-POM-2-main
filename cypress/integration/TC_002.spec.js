3ae0fc83a68d016802069a7675db424080504b03
saifulislammunna <saifulmunna28@gmail.com>
2022-01-27 10:25:53 +0600

    typed-text-added

--- cypress/integration/TC_002.spec.js
+++ cypress/integration/TC_002.spec.js
@@ -4,6 +4,10 @@
 
 
   it('login test', function(){
+    const typedText = 'saifulmunna28@gmail.com'
     cy.visit('https://identity.flickr.com/login?redir=https%3A%2F%2Fwww.flickr.com%2F')
+     cy.get('[data-testid="identity-email-input"]')
+     .type(typedText)
+     .should('have.value', typedText)
      
 })  
