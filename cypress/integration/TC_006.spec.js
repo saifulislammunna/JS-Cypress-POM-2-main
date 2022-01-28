a078098f9ac747e8d08ff2e46e28a931219ad761
saifulislammunna <saifulmunna28@gmail.com>
2022-01-28 11:04:11 +0600

    notifi-added

---
+++ cypress/integration/TC_006.spec.js
@@ -0,0 +1,29 @@
+it('Visits flickr.com', () => {
+    cy.visit('https://identity.flickr.com/account-picker')
+    cy.get('.py-2').click() 
+})
+
+ 
+it('login test', function(){
+    const typedText = 'saifulmunna28@gmail.com'
+    const typedPassword = '+P5*M,@cjPRz$Bm'  
+
+     
+
+     cy.get('[data-testid="identity-email-input"]')
+     .type(typedText)
+     .should('have.value', typedText)
+     cy.get('[data-testid="identity-form-submit-button"]').click()  
+     cy.get('[data-testid="identity-password-input"]') 
+     .type(typedPassword)
+     .should('have.value', typedPassword) 
+   
+      
+     
+})  
+
+it('Notification', function(){
+    cy.get('[data-testid="identity-form-submit-button"]').click()  
+    cy.get('.c-notification-icon').click() 
+
+})
